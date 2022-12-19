709080175f8b5db7bd60b47d0099a2691eee7745
Stylesp007 <82741858+Stylesp007@users.noreply.github.com>
2022-12-19 16:05:13 -0500

    Update app.js

--- JS-Weather-app/app.js
+++ JS-Weather-app/app.js
@@ -10,7 +10,6 @@ const temp = document.querySelector('.temp');
 button.addEventListener('click',() => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=`+inputValue.value+`&appid=b92ac238b9ac22eefac74477bd060b21`)
   .then(response => response.json())
-  // .then(data => console.log(data))
   .then(data => {
     const nameValue = data['name'];
     const tempVal   = data['main']['temp'];
